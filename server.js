8aa3ac961b07b8ec4c09c0bce05baf0e45ce6715
johnchuma <johnvchuma@gmail.com>
2025-07-29 14:33:03 +0300

    fix: server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const clientRoutes = require("./routes/clientRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 4001;
 
 // Middleware
 app.use(helmet());
